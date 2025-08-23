f72b64c0ee79f01aa5790f0ea231ce9534273176
KevinAlpizarDev <kevinalpizarde@gmail.com>
2025-08-22 21:18:47 -0600

    Fix

--- BackEnd/conecta2_back/settings.py
+++ BackEnd/conecta2_back/settings.py
@@ -73,7 +73,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'Conecta2',
         'USER': 'root',
-        'PASSWORD': 'PUTOamo1998',
+        'PASSWORD': 'tu_contraseña',
         'HOST': '127.0.0.1',
         'PORT':'3306'
     }
